a86bcec84408cbe3a83c38e3bc7d8b2c03b1d416
lexakay <raayone@googlemail.com>
2017-02-07 16:34:11 +0100

    Add files via upload

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SimpleTest {
+
+	@Test
+	public void simpleTest() {
+		int a = 1;
+		int b = 2;
+		assertTrue(a + b == 3);
+	}
+}
